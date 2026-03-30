d01ee0fd4d353b0122ff953540abd582b9294dda
jacobmarx24 <jacob.marx24@gmail.com>
2026-03-30 16:16:54 -0500

    relocating Main.java's file path to be more reasonable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 import jacobmarx.ReserveRoom.RoomSelectionUI;
 import jacobmarx.RoomsAndReservation.ReservationService;
 import jacobmarx.RoomsAndReservation.RoomService;
-
-
 import javax.swing.*;
 
 public class Main {
-
     public static void main(){
         //Create the ReservationService and RoomService
         //TODO: UI controller class
@@ -16,6 +13,4 @@ public class Main {
         //line below decides the opening UI. currently set to RoomSelection, change to main menu at some point
         SwingUtilities.invokeLater(RoomSelectionUI::createUI);
     }
-
-
 }
